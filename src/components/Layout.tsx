a18c92c4aa0487c130c6de119d23fb7a0f7fd8ab
arzoo rai <148741922+arzoo0511@users.noreply.github.com>
2026-03-15 19:29:37 +0530

    VERCEL

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import AnimatedBackground from './AnimatedBackground';
 import Navbar from "./Navbar";
+
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const loc = useLocation();
   const isLogin = loc.pathname === '/login';
 
   return (
     <div className="min-h-screen relative">
-      {/* Ocean animated background */}
       <AnimatedBackground />
-      {/* Navigation bar, hidden on login page */}
+
       {!isLogin && <Navbar />}
-      {/* Content layer with padding */}
+
       <main className="relative z-10 pt-20 pb-16 px-4 sm:px-6 lg:px-8">
         {children}
       </main>
